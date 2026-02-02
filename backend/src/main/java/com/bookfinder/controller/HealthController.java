c1e47e7a6cc7f98de2c8ea8a796d7e0a009c4b0b
nsatwik <satwik.sathu5@gmail.com>
2026-02-02 11:39:53 +0000

    cors_added

--- backend/src/main/java/com/bookfinder/controller/HealthController.java
+++ backend/src/main/java/com/bookfinder/controller/HealthController.java
@@ -1,9 +1,11 @@
 package com.bookfinder.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")  // 👈 TEMP: allow all
 public class HealthController {
 
     @GetMapping("/health")
